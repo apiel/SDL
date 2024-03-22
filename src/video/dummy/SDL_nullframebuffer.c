6b7dc36fa0f55a17d23a70d46b0a1efce55d65d7
Alexandre Piel <alexandre.piel@zooplus.com>
2024-03-22 20:15:40 +0100

    rgb

--- src/video/dummy/SDL_nullframebuffer.c
+++ src/video/dummy/SDL_nullframebuffer.c
@@ -177,6 +177,7 @@ int SDL_DUMMY_CreateWindowFramebuffer(_THIS, SDL_Window *window, Uint32 *format,
     return 0;
 }
 
+#define MY_RGB(r, g, b) (((r >> 3) << 11) | ((g >> 2) << 5) | (b >> 3))
 bool yodrawed = false;
 int SDL_DUMMY_UpdateWindowFramebuffer(_THIS, SDL_Window *window, const SDL_Rect *rects, int numrects)
 {
@@ -231,7 +232,8 @@ int SDL_DUMMY_UpdateWindowFramebuffer(_THIS, SDL_Window *window, const SDL_Rect 
             // pixel[0] = pixels[0] & 0xFF | (pixels[1] & 0xFF) << 8;
             // pixel[1] = pixels[2] & 0xFF | (pixels[3] & 0xFF) << 8;
 
-            rgb = ((pixels[0] & 0x0ff) << 16) | ((pixels[1] & 0x0ff) << 8) | (pixels[2] & 0x0ff);
+            // rgb = ((pixels[0] & 0x0ff) << 16) | ((pixels[1] & 0x0ff) << 8) | (pixels[2] & 0x0ff);
+            rgb = MY_RGB(pixels[0], pixels[1], pixels[2]);
             pixel[0] = (uint8_t)(rgb >> 8);
             pixel[1] = (uint8_t)(rgb & 0xFF);
 
