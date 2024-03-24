2a35679d915477a5778106419c786c9d227a0e45
Alexandre Piel <alexandre.piel@zooplus.com>
2024-03-24 19:14:24 +0100

    st

--- src/video/dummy/SDL_nullframebuffer.c
+++ src/video/dummy/SDL_nullframebuffer.c
@@ -261,12 +261,12 @@ int SDL_DUMMY_UpdateWindowFramebuffer(_THIS, SDL_Window *window, const SDL_Rect 
             // rgb = (((pixels[0] >> 3) << 11) | ((pixels[1] >> 2) << 5) | (pixels[2] >> 3));
             rgb = ((pixels[0] & 0xF8) << 8) | ((pixels[1] & 0xFC) << 3) | (pixels[2] >> 3);
             pixelsBuffer[y] = (uint8_t)(rgb >> 8);
-            pixel[y + 1] = (uint8_t)(rgb & 0xFF);
+            pixelsBuffer[y + 1] = (uint8_t)(rgb & 0xFF);
         }
 
         // Let's rotate 90 degrees
         sendAddr(DISPLAY_SET_CURSOR_Y, (uint16_t)x, (uint16_t)x);
-        sendAddr(DISPLAY_SET_CURSOR_X, (uint16_t)0, (uint16_t)h);
+        sendAddr(DISPLAY_SET_CURSOR_X, (uint16_t)0, (uint16_t)h - 1);
         sendCmd(DISPLAY_WRITE_PIXELS, pixelsBuffer, size);
     }
 
