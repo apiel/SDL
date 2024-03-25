49944e5801463bea136ba1e1326979a8716e0db9
Alexandre Piel <alexandre.piel@zooplus.com>
2024-03-25 22:25:25 +0100

    try to fix

--- src/video/st7789/config.h
+++ src/video/st7789/config.h
@@ -1,4 +1,6 @@
 #pragma once
+#ifndef _ST7789_CONFIG_H_
+#define _ST7789_CONFIG_H_
 
 #define GPIO_TFT_DATA_CONTROL 6
 #define GPIO_TFT_RESET_PIN 5
@@ -7,3 +9,5 @@
 
 #define DISPLAY_WIDTH 240
 #define DISPLAY_HEIGHT 240
+
+#endif
