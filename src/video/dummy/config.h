fd146a86141fbe61f2edab946f9fbc11bdd1faeb
Alexandre Piel <alexandre.piel@zooplus.com>
2024-03-25 21:40:09 +0100

    cleanup

--- src/video/dummy/config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define GPIO_TFT_DATA_CONTROL 6
-#define GPIO_TFT_RESET_PIN 5
-#define GPIO_TFT_BACKLIGHT 13
-#define SPI_BUS_CLOCK_DIVISOR 20
-
-#define DISPLAY_WIDTH 240
-#define DISPLAY_HEIGHT 240
