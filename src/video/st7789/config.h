a67e72310ccad127de57c03234a8a7bfbd27bc6d
Alexandre Piel <alexandre.piel@zooplus.com>
2024-03-26 20:48:30 +0100

    st

---
+++ src/video/st7789/config.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef _ST7789_CONFIG_H_
+#define _ST7789_CONFIG_H_
+
+#define GPIO_TFT_DATA_CONTROL 6
+#define GPIO_TFT_RESET_PIN 5
+#define GPIO_TFT_BACKLIGHT 13
+#define SPI_BUS_CLOCK_DIVISOR 20
+
+#define DISPLAY_WIDTH 240
+#define DISPLAY_HEIGHT 240
+
+#endif
